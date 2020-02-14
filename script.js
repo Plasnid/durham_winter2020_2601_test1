82b0566e83560dc0a4c743c05e9e77e19a8358df
Plasnid <stuart@sackler.ca>
2020-02-14 13:58:43 -0500

    questions updated

---
+++ script.js
@@ -0,0 +1,72 @@
+//Lunchtime a test experience  Total points for the test: 48 
+
+/**
+ * q1.  Fries with a side of variables: make a variable called specialFries which is an object (total 11 points)
+ * 
+ * Add the following values to the specialFries object
+ * 
+ * a. add a value called friesName that holds a string which names your fries
+ * b. add a value called numberOfFries that holds a number....the number of fries you want to have
+ * c. add a value called isSpicy that holds a boolean, that tells whether your fries are spicy or not
+ * d. add a value called dips that holds an array of 3 strings.  Each string should be the name of a dip for your fries
+ */
+
+/**
+ * q2.  Fountain drink: Create a function declaration (total 8 points)
+ * 
+ * Create a function declaration that takes in a beverage and a size.  
+ * Your function should console.log a template literal with the following structure
+ * 
+ * A "size" cup of "beverage"!
+ * 
+ * Call your function so the message displays in the console of your browser
+ */
+
+/**
+ * q3.  Sandwiches are delicious:  Its time to output sandwich from an array! (total 10 points)
+ * 
+ * Create a function expression.  Give the function expression the array below as a parameter.
+ * 
+ * use push to add "mushrooms to the array"
+ * 
+ * use splice to switch "mustard" for "hot sauce"
+ * 
+ * Use a for loop to console log the array.
+ */
+
+let yummySandwich = [
+    "whole wheat bread",
+    "mayo", 
+    "mustard", 
+    "pickles", 
+    "lettuce", 
+    "grilled haloumi cheese"
+];
+
+/**
+ * q4.  Dessert first!  Use a for loop that runs from the last entry to the first (total 5 points)
+ * 
+ * Output the meal array below to the console log from the last entry to the first entry
+ */
+
+let tastingMenu = [
+    "Caesar Salad",
+    "Gespacio Soup",
+    "Cucumber Sorbet",
+    "Catch of the Day",
+    "Amuse Buche",
+    "Creme Brulee"
+];
+
+/**
+ * q5. Writing the menu to the html page (total 14 points)
+ * 
+ * a. add an h1 tag to the page using javascript with the following text "Chefs Table" (3 points)
+ * 
+ * b. add an h2 tag to the page using javascript with the following text "A Celebration Of Taste" (3 points)
+ * 
+ * c.  add a ul to the page using javascript (2 points)
+ * 
+ * d.  use a for loop to add the contents of the tastingMenu array to the ul tag with javascript ( 6 points)
+ * 
+ */
